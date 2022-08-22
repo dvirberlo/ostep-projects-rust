e3ec7a5262a9c25e150297210c0d4dac4a317136
dvirberlo <dvir.berl@gmail.com>
2022-08-22 21:11:20 +0000

    refactor wgrep

--- src/initial-utilities/wgrep/wgrep.rs
+++ src/initial-utilities/wgrep/wgrep.rs
@@ -1,13 +1,13 @@
-use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Lines;
 
 const HELP_MSG: &str = "wgrep: searchterm [file ...]\n";
 const ERROR_MSG: &str = "wgrep: cannot open file\n";
 
 fn main() -> Result<(), std::io::Error> {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     let args_len = args.len();
     if args_len < 2 {
         print!("{}", HELP_MSG);
@@ -21,17 +21,8 @@ fn main() -> Result<(), std::io::Error> {
 
 fn grep_stdin(search_term: &str) -> Result<(), std::io::Error> {
     let stdin = std::io::stdin();
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(line) => {
-                if line.contains(search_term) {
-                    print!("{}\n", line);
-                }
-            }
-            Err(_) => {}
-        }
-    }
-    Ok(())
+    let mut lines = stdin.lock().lines();
+    return grep_lines(search_term, &mut lines);
 }
 
 fn grep_file(search_term: &str, filename: &str) -> Result<(), std::io::Error> {
@@ -43,7 +34,12 @@ fn grep_file(search_term: &str, filename: &str) -> Result<(), std::io::Error> {
         }
     };
     let reader = BufReader::new(file);
-    for line in reader.lines() {
+    let mut lines = reader.lines();
+    return grep_lines(search_term, &mut lines);
+}
+
+fn grep_lines<T: BufRead>(search_term: &str, lines: &mut Lines<T>) -> Result<(), std::io::Error>{
+    for line in lines {
         match line {
             Ok(line) => {
                 if line.contains(search_term) {
